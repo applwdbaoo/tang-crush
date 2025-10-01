52a6213a78f99707f05c153b283e824c3841290a
applwdbaoo <meomeo9999.meo@gmail.com>
2025-10-01 21:42:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,13 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên ',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Vẫn cứ là thích cậu <33',
+    btnNo: 'Không cậu ơiii :3',
+    question:'cậu có thích tui thật hongg <3',
+    btnReply: 'Gửi cho tuii <3',
+    reply: 'thật mòoo <33333333',
+    mess: 'tui biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h tui qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
